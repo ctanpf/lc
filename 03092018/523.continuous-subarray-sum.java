595c16398a60f329915217c1332b08bd0d6dc9ac
cliff <clifftanpeifeng@gmail.com>
2018-03-10 10:09:14 -0600

    🙂

--- 03092018/523.continuous-subarray-sum.java
+++ 03092018/523.continuous-subarray-sum.java
@@ -46,6 +46,15 @@
  */
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        
+        if (nums.length == 0) return false;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                if (k != 0 && sum % k == 0) return true;
+                if (k == 0 && sum == 0) return true;
+            }
+        }
+        return false;
     }
 }
