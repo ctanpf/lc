913c652c57b860ec55f81df12feab827348dedf9
Cliff Tan <Cliff.Tan@veritas.com>
2018-03-16 09:33:37 -0500

    😛

---
+++ 2/03172018/291.word-pattern-ii.java
@@ -0,0 +1,33 @@
+/*
+ * [291] Word Pattern II
+ *
+ * https://leetcode.com/problems/word-pattern-ii/description/
+ *
+ * algorithms
+ * Hard (38.50%)
+ * Total Accepted:    21.5K
+ * Total Submissions: 56K
+ * Testcase Example:  '"abab"\n"redblueredblue"'
+ *
+ * Given a pattern and a string str, find if str follows the same pattern.
+ * ⁠Here follow means a full match, such that there is a bijection between a
+ * letter in pattern and a non-empty substring in str.
+ * 
+ * Examples:
+ * 
+ * pattern = "abab", str = "redblueredblue" should return true.
+ * pattern = "aaaa", str = "asdasdasdasd" should return true.
+ * pattern = "aabb", str = "xyzabcxzyabc" should return false.
+ * 
+ * 
+ * 
+ * 
+ * Notes:
+ * You may assume both pattern and str contains only lowercase letters.
+ * 
+ */
+class Solution {
+    public boolean wordPatternMatch(String pattern, String str) {
+        
+    }
+}
