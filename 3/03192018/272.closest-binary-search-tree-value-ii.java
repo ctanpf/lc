ff44beb94302d92ac1369151ef4e4d20249fe853
cliff <clifftanpeifeng@gmail.com>
2018-03-18 21:42:56 -0500

    🍔

---
+++ 3/03192018/272.closest-binary-search-tree-value-ii.java
@@ -0,0 +1,41 @@
+/*
+ * [272] Closest Binary Search Tree Value II
+ *
+ * https://leetcode.com/problems/closest-binary-search-tree-value-ii/description/
+ *
+ * algorithms
+ * Hard (39.93%)
+ * Total Accepted:    22.2K
+ * Total Submissions: 55.7K
+ * Testcase Example:  '[1]\n0.000000\n1'
+ *
+ * 
+ * Given a non-empty binary search tree and a target value, find k values in
+ * the BST that are closest to the target.
+ * 
+ * Note:
+ * 
+ * Given target value is a floating point.
+ * You may assume k is always valid, that is: k ≤ total nodes.
+ * You are guaranteed to have only one unique set of k values in the BST that
+ * are closest to the target.
+ * 
+ * Follow up:
+ * Assume that the BST is balanced, could you solve it in less than O(n)
+ * runtime (where n = total nodes)?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        
+    }
+}
