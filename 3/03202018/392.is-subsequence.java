ea36c220e7871e99e43e23146c9a70c236cc9a74
Cliff Tan <Cliff.Tan@veritas.com>
2018-03-20 13:00:06 -0500

    👌

--- 3/03202018/392.is-subsequence.java
+++ 3/03202018/392.is-subsequence.java
@@ -49,6 +49,13 @@
  */
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
+        if (s == null || s.length() == 0) return true;
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) j++;
+            if (j == s.length()) return true;
+        }
+        return j >= s.length();
     }
 }
