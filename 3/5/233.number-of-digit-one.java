ec682e82a329f2f971d89db4422780a469bb5f2d
Cliff Tan <Cliff.Tan@veritas.com>
2018-03-20 13:26:35 -0500

    😴

---
+++ 3/5/233.number-of-digit-one.java
@@ -0,0 +1,26 @@
+/*
+ * [233] Number of Digit One
+ *
+ * https://leetcode.com/problems/number-of-digit-one/description/
+ *
+ * algorithms
+ * Hard (28.97%)
+ * Total Accepted:    33.7K
+ * Total Submissions: 116.2K
+ * Testcase Example:  '-1'
+ *
+ * Given an integer n, count the total number of digit 1 appearing in all
+ * non-negative integers less than or equal to n.
+ * 
+ * 
+ * For example: 
+ * Given n = 13,
+ * Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12,
+ * 13.
+ * 
+ */
+class Solution {
+    public int countDigitOne(int n) {
+        
+    }
+}
