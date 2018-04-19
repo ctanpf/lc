c5aa4715ba24b4e2619cbbfef01f4fda6fd9ead5
cliff <clifftanpeifeng@gmail.com>
2018-04-18 23:45:06 -0500

    😦

---
+++ 300/144.binary-tree-preorder-traversal.java
@@ -0,0 +1,44 @@
+/*
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (46.93%)
+ * Total Accepted:    227.9K
+ * Total Submissions: 485.7K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * 
+ * For example:
+ * Given binary tree [1,null,2,3],
+ * 
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * 
+ * 
+ * return [1,2,3].
+ * 
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+    }
+}
---
+++ 300/150.evaluate-reverse-polish-notation.java
@@ -0,0 +1,31 @@
+/*
+ * [150] Evaluate Reverse Polish Notation
+ *
+ * https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+ *
+ * algorithms
+ * Medium (28.50%)
+ * Total Accepted:    114.7K
+ * Total Submissions: 402.5K
+ * Testcase Example:  '["2","1","+","3","*"]'
+ *
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * 
+ * Valid operators are +, -, *, /. Each operand may be an integer or another
+ * expression.
+ * 
+ * Some examples:
+ * 
+ * 
+ * ⁠ ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ * ⁠ ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int evalRPN(String[] tokens) {
+        
+    }
+}
