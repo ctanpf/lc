75c7f24159d8fde97ed58621f27d26bf2aa4ba8a
Cliff Tan <Cliff.Tan@veritas.com>
2018-04-23 23:32:38 -0500

    👽

---
+++ 300/166.fraction-to-recurring-decimal.java
@@ -0,0 +1,36 @@
+/*
+ * [166] Fraction to Recurring Decimal
+ *
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/description/
+ *
+ * algorithms
+ * Medium (18.12%)
+ * Total Accepted:    64.1K
+ * Total Submissions: 353.5K
+ * Testcase Example:  '1\n2'
+ *
+ * Given two integers representing the numerator and denominator of a fraction,
+ * return the fraction in string format.
+ * 
+ * If the fractional part is repeating, enclose the repeating part in
+ * parentheses.
+ * 
+ * For example,
+ * 
+ * 
+ * Given numerator = 1, denominator = 2, return "0.5".
+ * Given numerator = 2, denominator = 1, return "2".
+ * Given numerator = 2, denominator = 3, return "0.(6)".
+ * 
+ * 
+ * 
+ * 
+ * Credits:
+ * Special thanks to @Shangrila for adding this problem and creating all test
+ * cases.
+ * 
+ */
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+    }
+}
---
+++ 300/187.repeated-dna-sequences.java
@@ -0,0 +1,32 @@
+/*
+ * [187] Repeated DNA Sequences
+ *
+ * https://leetcode.com/problems/repeated-dna-sequences/description/
+ *
+ * algorithms
+ * Medium (33.09%)
+ * Total Accepted:    94.2K
+ * Total Submissions: 284.8K
+ * Testcase Example:  '"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"'
+ *
+ * 
+ * All DNA is composed of a series of nucleotides abbreviated as A, C, G, and
+ * T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to
+ * identify repeated sequences within the DNA.
+ * 
+ * Write a function to find all the 10-letter-long sequences (substrings) that
+ * occur more than once in a DNA molecule.
+ * 
+ * 
+ * For example,
+ * 
+ * Given s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",
+ * 
+ * Return:
+ * ["AAAAACCCCC", "CCCCCAAAAA"].
+ * 
+ */
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+    }
+}
