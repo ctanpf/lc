8836dffe032fd88ee1c19d34fd8d09f3473cf9b8
cliff <clifftanpeifeng@gmail.com>
2018-05-04 00:00:35 -0500

    😤

--- 300/2/398.random-pick-index.java
+++ 300/2/398.random-pick-index.java
@@ -36,13 +36,24 @@
  * 
  */
 class Solution {
-
+    int[] nums;
+    Random rand;
     public Solution(int[] nums) {
-        
+        this.nums = nums;
+        rand = new Random();
     }
     
     public int pick(int target) {
-        
+       int count = 0;
+       int res = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (target == nums[i]) {
+                count++;
+            }
+            else continue;
+            if (rand.nextInt(count) == 0) res = i;
+       }
+       return res;
     }
 }
 
